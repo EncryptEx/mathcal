1372581c5a1dc3e180de7ed33ba5d10fa8900618
EncryptEx <limusina10@gmail.com>
2023-08-12 23:15:34 +0200

    Formalize into function

--- index.php
+++ index.php
@@ -1,32 +1,44 @@
-<?php 
-
-$date = new DateTime('now');
-$daysOfLoop = 1000;
+<?php
 
 
-for ($i=0; $i < $daysOfLoop; $i++) { 
 
-    
-    $date = date_add($date, new DateInterval('P1D')); # Add 1 day
+function getMathDays(DateTime $start, int $iterationDays, bool $findOnlyFirst = False)
+{
 
-    $day = (int)($date->format('d'));
-    $month = (int)($date->format('m'));
-    $year = (int)($date->format('y'));
-    if($day + $month == $year) {
-    echo $date->format("l jS \of F Y") . " is a mathematical day! --> " . $day . "+" . $month . "=" . $year . "\n";
-    }
-    if(-$day + $month == $year) {
-        echo $date->format("l jS \of F Y") . " is a mathematical day! --> -" . $day . "+" . $month . "=" . $year . "\n";
+    $date = $start;
+    $endNow = false;
+    for ($i = 0; $i < $iterationDays; $i++) {
+        $date = date_add($date, new DateInterval('P1D')); # Add 1 day
+
+        $day = (int)($date->format('d'));
+        $month = (int)($date->format('m'));
+        $year = (int)($date->format('y'));
+
+        if ($day + $month == $year) {
+            echo $date->format("l jS \of F Y") . " is a mathematical day! --> " . $day . "+" . $month . "=" . $year . "\n";
+            $endNow = true;
         }
-    if($day - $month == $year) {
-        echo $date->format("l jS \of F Y") . " is a mathematical day! --> " . $day . "-" . $month . "=" . $year . "\n";
-    }
-    if($day * $month == $year) {
-        echo $date->format("l jS \of F Y") . " is a mathematical day! --> " . $day . "*" . $month . "=" . $year . "\n";
-    }
-    if($day / $month == $year) {
-        echo $date->format("l jS \of F Y") . " is a mathematical day! --> " . $day . "/" . $month . "=" . $year . "\n";
-    }
-    
+        if (-$day + $month == $year) {
+            echo $date->format("l jS \of F Y") . " is a mathematical day! --> -" . $day . "+" . $month . "=" . $year . "\n";
+            $endNow = true;
+        }
+        if ($day - $month == $year) {
+            echo $date->format("l jS \of F Y") . " is a mathematical day! --> " . $day . "-" . $month . "=" . $year . "\n";
+            $endNow = true;
+        }
+        if ($day * $month == $year) {
+            echo $date->format("l jS \of F Y") . " is a mathematical day! --> " . $day . "*" . $month . "=" . $year . "\n";
+            $endNow = true;
+        }
+        if ($day / $month == $year) {
+            echo $date->format("l jS \of F Y") . " is a mathematical day! --> " . $day . "/" . $month . "=" . $year . "\n";
+            $endNow = true;
+        }
 
+        if($findOnlyFirst && $endNow){
+            return;
+        }
+    }
 }
+
+getMathDays(new DateTime('now'), 1000, true);
